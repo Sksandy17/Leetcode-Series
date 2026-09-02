905200fe1a7d030b2b7149f8ca0513ff22f27880
Sksandy17 <sandhiyacse2006@gmail.com>
2026-09-02 21:28:44 +0530

    Sort Array By Parity.java

--- Sort Array By Parity.java
+++ Sort Array By Parity.java
@@ -2,14 +2,14 @@ class Solution {
     public int[] sortArrayByParity(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
-        while (left < right) {
-            while (left < right && nums[left] % 2 == 0) {
+        while(left<right){
+            while(left<right && nums[left]%2 == 0){
                 left++;
             }
-            while (left < right && nums[right] % 2 != 0) {
+            while(left<right && nums[right]%2 != 0){
                 right--;
             }
-            if (left < right) {
+            if(left<right){
                 int temp = nums[left];
                 nums[left] = nums[right];
                 nums[right] = temp;
